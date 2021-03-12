7705791db2a80d5ea800a02858ce6643d0f8a84b
heqikai <1299135371@qq.com>
2021-03-12 02:59:03 -0500

    ad

--- 2.c
+++ 2.c
@@ -2,6 +2,19 @@
 
 int main()
 {
+	int a,b;
+	scanf("%d%d",&a,&b);
+	if ((a%400==0)||(a%4==0&&a%100!=0))
+	{
+		if(b==1||b==3||b==5||b==7||b==8||b==10||b==12)
+		{
+			printf("")
+		}
+	}
+	else
+	{
+
+	}
 	printf("heqikaishuai2");
 	return 0;
 }
